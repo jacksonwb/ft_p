485e048042e86c29e7e29d6d620425db5f8ac8b6
jacksonwb <jbeall@e1z3r1p1.42.us.org>
2019-07-29 20:35:21 -0700

    norm client header

--- include/client.h
+++ include/client.h
@@ -6,7 +6,7 @@
 /*   By: jbeall <jbeall@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/15 16:53:48 by jbeall            #+#    #+#             */
-/*   Updated: 2019/07/29 20:17:21 by jbeall           ###   ########.fr       */
+/*   Updated: 2019/07/29 20:34:34 by jbeall           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,30 +44,30 @@ typedef struct	s_client_cmd
 /*
 ** client_handler
 */
-void	handle_ls(int sfd, char **av);
-void	handle_pwd(int sfd, char **av);
-void	handle_cd(int sfd, char **av);
-void	handle_get(int sfd, char **av);
-void	handle_put(int sfd, char **av);
-void	handle_quit(int sfd, char **av);
-void	handle_lls(int sfd, char **av);
-void	handle_lcd(int sfd, char **av);
-void	handle_lpwd(int sfd, char **av);
+void			handle_ls(int sfd, char **av);
+void			handle_pwd(int sfd, char **av);
+void			handle_cd(int sfd, char **av);
+void			handle_get(int sfd, char **av);
+void			handle_put(int sfd, char **av);
+void			handle_quit(int sfd, char **av);
+void			handle_lls(int sfd, char **av);
+void			handle_lcd(int sfd, char **av);
+void			handle_lpwd(int sfd, char **av);
 
 /*
 ** utility
 */
-void	write_progress(size_t sent, size_t total);
-void	clear_progress(void);
-void	usage(void);
-bool	prompt(void);
-void	write_progress(size_t sent, size_t total);
-void	clear_progress(void);
-int		client_connect(char *addr_str, char *port_str);
+void			write_progress(size_t sent, size_t total);
+void			clear_progress(void);
+void			usage(void);
+bool			prompt(void);
+void			write_progress(size_t sent, size_t total);
+void			clear_progress(void);
+int				client_connect(char *addr_str, char *port_str);
 
-size_t	read_data_from_request(int sfd, t_client_cmd cmd,
+size_t			read_data_from_request(int sfd, t_client_cmd cmd,
 	int outfd, size_t file_size);
-size_t	send_data_for_request(int sfd, t_client_cmd cmd,
+size_t			send_data_for_request(int sfd, t_client_cmd cmd,
 	int infd, size_t file_size);
 
 #endif
